a266d1a9354347dc94bd670f4edb89f26030978e
Ben Therien <cs135cid@yahoo.com>
2019-03-31 21:18:21 -0400

    Create GetFocusWindowTitle.ps1

---
+++ GetFocusWindowTitle.ps1
@@ -0,0 +1,17 @@
+﻿$code = @'
+    [DllImport("user32.dll")]
+     public static extern IntPtr GetForegroundWindow();
+'@
+
+Add-Type $code -Name Utils -Namespace Win32
+
+while(1){
+    $Script:hwnd = [Win32.Utils]::GetForegroundWindow()
+    Get-Process | 
+        Where-Object { $_.mainWindowHandle -eq $hwnd } | 
+        Select-Object processName, MainWindowTItle, MainWindowHandle,Id
+    sleep -Milliseconds 200
+}
+
+$Script:hwnd = [Win32.Utils]::GetForegroundWindow()
+[Win32.NativeMethods]::ShowWindowAsync($hwnd, 2)# Minimize window
